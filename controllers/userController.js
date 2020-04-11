528f26126340f42df31080dbc3868e38d0ce90b6
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-10 20:45:10 -0500

    user's visited states populates in user object + see notes in user show page for where I got stuck trying to render (couldnt define state, states, etc.)

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ const User = require('../models/user')
 
 
 router.get('/:userId', async (req, res, next) => {
-	// res.send('this is the user show page)   state: ({})
+
 	try {
 		const user = await User.findById(req.params.userId).populate('states')
 		console.log(user, 'this is the user') ///taking out this populate will render but not print user)
@@ -20,51 +20,33 @@ router.get('/:userId', async (req, res, next) => {
 	}
 })
 
-//NEED TO FIND OUT HOW TO REFERENCES USER'S STATES FROM THE USER MODEL IN THE CONTROLLER
+
 // post new state to user
 router.put('/:userId', async (req, res, next) => {
-	// try {
-		
-	// 	// const stateToAddToShowPage = {states: req.body.states}
-	// 	// console.log(stateToAddToShowPage)
-	// 	console.log(req.body.state)
-	// 	const statesVisited = await User.findById(req.params.userId).populate('states')
-	// 	console.log(statesVisted)
-	// 	user.states.push(statesVisited)
-	// 	await user.save()
-	// 	// res.send('user show page')
-	// 	res.render('users/show.ejs',{
-	// 		states: req.body.state
-	// 	})
-	// } catch(error) {
-	//   next(error)
-	// }
-	res.send('this works')
-
+	try {
+		const stateVisited = {
+			state: req.body.state, 
+			userId: req.session.user}
+		const user = await User.findById(req.params.userId).populate('states')
+		user.states.push(stateVisited)
+		await user.save()
+		res.locals.user = req.session.user
+		res.render('users/show.ejs', {
+			users: req.session.user,
+			// states: req.body.state,
+			// state: req.body.state,
+			// states: req.params.stateId,
+			// res.locals.states = req.session.states
+			
+		})
+	} catch(error) {
+	  next(error)
+	}
 })
 
+	
 
-// // EXPERIMENTAL POST ROUTE:
-// router.post('/:userId', async (req, res, next) => {
-//   try {
-//     // res.send('route working')
-//     const stateClickedOn = req.body.state
-//     console.log(req.session.userId)   //PRINTS
-//     console.log(req.session.user) //PRINTS
-//     const stateVisited = await State.findById(req.body.id)
-//     // const stateVisited = await State.findById(req.params.states)
-//     console.log(stateVisited, 'here is the state visited')
-//     console.log(req.body.states)
-//     const user = await User.findById(req.params.userId).populate('states')
-//     console.log(req.body.states)
-//     user.states.push(stateVisited)
-//     await user.save()
-//     res.send('user show page')
-//     res.render('users/show.ejs')
-//   } catch(error) {
-//     next(error)
-//   }
-// })
+
 
 
 
